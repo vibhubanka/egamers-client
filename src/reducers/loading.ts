3d4d664a4305ad172164e3e13beeec82cb6d7294
Vibhu Banka <v.banka93@gmail.com>
2020-08-24 02:19:08 +0530

    Fixes issue with loading state prop name, rename vars

--- src/reducers/loading.ts
+++ src/reducers/loading.ts
@@ -5,25 +5,25 @@ interface LoadingState {
   [key: string]: boolean;
 }
 
-const r = /(.+)_REQUEST$/;
-const s = /(.+)_SUCCESS$/;
-const f = /(.+)_FAILURE$/;
+const req = /(.+)_REQUEST$/;
+const success = /(.+)_SUCCESS$/;
+const fail = /(.+)_FAILURE$/;
 
 const indicatorName = (pattern: RegExp, type: Action['type']) => {
   let arr = pattern.exec(type);
-  return arr ? camelize(arr[1]) : '';
+  return arr ? camelize(arr[1].toLowerCase()) : '';
 };
 
-export default function loadingReducer(state: LoadingState, action: Action): LoadingState {
+export default function loadingReducer(state: LoadingState = {}, action: Action): LoadingState {
   const { type } = action;
 
   switch (true) {
-    case r.test(type):
-      return { ...state, [indicatorName(r, type)]: true };
-    case s.test(type):
-      return { ...state, [indicatorName(s, type)]: true };
-    case f.test(type):
-      return { ...state, [indicatorName(f, type)]: true };
+    case req.test(type):
+      return { ...state, [indicatorName(req, type)]: true };
+    case success.test(type):
+      return { ...state, [indicatorName(success, type)]: false };
+    case fail.test(type):
+      return { ...state, [indicatorName(fail, type)]: false };
     default:
       return state;
   }

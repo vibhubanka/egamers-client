5d3bccfb6813c067690d2325c978d6f0afd67cc6
Vibhu Banka <v.banka93@gmail.com>
2020-08-24 02:39:03 +0530

    Removes redundant files, minor fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(

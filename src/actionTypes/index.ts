8fb336c603f0e3a8dfe74e5acedc13b588199ed5
Vibhu Banka <v.banka93@gmail.com>
2020-09-01 23:43:22 +0530

    Update imports

--- src/actionTypes/index.ts
+++ src/actionTypes/index.ts
@@ -1,2 +1,7 @@
+import * as U from './user';
+import * as C from './common';
+import * as T from './tournaments';
+export * from './common';
 export * from './tournaments';
 export * from './user';
+export type AppAction = C.ModalAction | T.TournamentAction | U.UserAction;

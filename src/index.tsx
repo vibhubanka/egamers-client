0b341e0d76055700f0fbffad0a1669af1622bb36
Vibhu Banka <v.banka93@gmail.com>
2020-09-01 23:34:00 +0530

    Add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
+import { Global } from '@emotion/core';
+import globalStyles from 'utils/styles/global';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
+      <Global styles={globalStyles} />
+
       <App />
     </React.StrictMode>
   </Provider>,

4dc9a4affae6b4e0499e037a061501e3365bd928
Vibhu Banka <v.banka93@gmail.com>
2020-09-01 23:47:56 +0530

    Integrate navigation, routes, modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
-import Navbar from 'components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import styled from '@emotion/styled';
+import Navigation from 'components/Navigation';
+import Modal from 'components/Modal';
+import Routes from 'routes';
+
+const AppContainer = styled.div`
+  background-color: var(--dark);
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <AppContainer className="app">
+      <Router>
+        <Navigation />
+
+        <Routes />
+
+        <Modal />
+      </Router>
+    </AppContainer>
   );
 }
 
